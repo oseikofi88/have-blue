c05a069d3a87f2ae0ecff69be5476b584ea3f63d
RIchard <richard@greyparror.io>
2021-09-30 22:12:50 +0000

    add twitter have blue dependencies

---
+++ twitter/src/main/java/wima/works/havebluetwitter/HaveBlueTwitterApplication.java
@@ -0,0 +1,13 @@
+package wima.works.havebluetwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveBlueTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaveBlueTwitterApplication.class, args);
+	}
+
+}
---
+++ twitter/src/test/java/wima/works/havebluetwitter/HaveBlueTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package wima.works.havebluetwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaveBlueTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
