a142aac234452c4828421cc9969042cb2dcf6929
RIchard <richard@greyparror.io>
2021-10-09 21:22:15 +0000

    add google search router

--- orchestrator/src/main/java/io/greyparrot/RestRouter.java
+++ orchestrator/src/main/java/io/greyparrot/RestRouter.java
@@ -3,6 +3,7 @@ package io.greyparrot;
 import io.greyparrot.responseObjects.HealthCheckResponse;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestParamType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,15 +14,22 @@ public class RestRouter extends RouteBuilder {
     @Override
     public void configure() {
 
+
         restConfiguration()
                 .component("servlet")
-                //.bindingMode(RestBindingMode.json)
                 .producerComponent("http");
 
         rest()
                 .get("/").route().bean(HealthCheckResponse.class)
+                .endRest()
+                .get("/search")
+                .param().name("searchPhrase").type(RestParamType.header).required(true).endParam()
+                .route()
+                .log("Received search phrase is $simple{header.searchPhrase}")
+                .removeHeaders("CamelHttp*")
+                .setBody(simple("${header.searchPhrase}"))
+                .to("direct:google")
                 .endRest();
-
     }
 
 }
