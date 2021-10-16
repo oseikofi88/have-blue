9d9bb47217eedbcfdd3fff2f31a84da4a648670e
RIchard <richard@greyparror.io>
2021-10-16 15:58:14 +0000

    make processing parallel

--- orchestrator/src/main/java/io/greyparrot/Routes/AWSRekognition.java
+++ orchestrator/src/main/java/io/greyparrot/Routes/AWSRekognition.java
@@ -15,24 +15,18 @@ public class AWSRekognition extends RouteBuilder {
         from("rabbitmq:image_links?queue=image_links&autoDelete=false")
                 .log("${body}")
                 .log("${header.numberOfLinks}")
+                .log("${header.traceId}")
                 .setHeader("Accept", simple("binary"))//Change it according to the file content
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .split()
                 .jsonpath("$")
                 .streaming()
                 .parallelProcessing()
-//                .parallelAggregate()
-                .log("Split performed")
                 .toD("${body}")
-                .toD("file:images?fileName=${header.CamelSplitIndex}-${header.traceID}-"+ LocalDateTime.now())
-                .log("Done")
-                //put it in folder
-                .end();
+                .toD("file:images?fileName=${header.CamelSplitIndex}-${header.traceId}-"+ LocalDateTime.now())
+                .end()
+                .log("Done downloading of images");
 
-    }
-
-    private AggregationStrategy batchAggregationStrategy() {
-        return new ArrayListAggregationStrategy();
     }
 
 }
