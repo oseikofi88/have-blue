50353276d194e38b59d2a48800c50e9b58fa3acd
RIchard <richard@greyparror.io>
2022-07-20 19:49:27 +0000

    add twitter webhook endpoint

--- src/main/java/works/wima/RestRouter.java
+++ src/main/java/works/wima/RestRouter.java
@@ -1,10 +1,20 @@
 package works.wima;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.logging.Log;
 import works.wima.Routes.HealthCheckResponse;
 import works.wima.Routes.Service;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestParamType;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 
 @Component
@@ -36,8 +46,34 @@ public class RestRouter extends RouteBuilder {
                 .removeHeaders("CamelHttp*")
                 .setBody().method(Service.class, "formatResponse(${header.results})")
                 .marshal().json()
+                .endRest()
+                .get("/webhook/twitter")
+                .param().name("crc_token").type(RestParamType.header).required(true).endParam()
+                .route()
+                .log("${header.crc_token}")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        String twitterCrcToken = exchange.getIn().getHeader("crc_token").toString();
+                        String consumerSecret = System.getenv("TWITTER_API_SECRET");
+                        try {
+                            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+                            SecretKeySpec secretKey = new SecretKeySpec(consumerSecret.getBytes(), "HmacSHA256");
+                            sha256_HMAC.init(secretKey);
+                            String hash = "sha256="+ Base64.encodeBase64String(sha256_HMAC.doFinal(twitterCrcToken.getBytes()));
+
+                            ObjectMapper mapper = new ObjectMapper();
+                            ObjectNode response = mapper.createObjectNode();
+                            response.put("response_token", hash);
+                            String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(response);
+                            exchange.getIn().setBody(json);
+                        }
+                        catch (Exception e){
+                            log.error("There was an error in producing the response token" + e.getMessage());
+                        }
+                    }
+                })
                 .endRest();
     }
-
 }
 
